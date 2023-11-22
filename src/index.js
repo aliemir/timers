799ab4e84b6141fd1603a3caaaf7429f7c976b6a
Ali Emir Şen <senaliemir@gmail.com>
2023-11-22 23:43:50 +0300

    chore: update timestamps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import express, { urlencoded } from "express";
+import dayjs from "dayjs";
+import fs from "fs/promises";
 
 const app = express();
+
+const START_DATE_CONSTRAINT = new Date("2023-02-09T00:00:00.000Z").getTime();
+
+const nowWithOffset = () => {
+  return Date.now() - START_DATE_CONSTRAINT;
+};
+
+const formattedTime = (duration) => {
+  const days = Math.floor(duration / 1000 / 60 / 60 / 24);
+  const hours = Math.floor((duration / 1000 / 60 / 60) % 24);
+  const minutes = Math.floor((duration / 1000 / 60) % 60);
+  const seconds = Math.floor((duration / 1000) % 60);
+
+  let str = "";
+
+  if (days > 0) str += `${days} day${days === 1 ? "" : "s"} `;
+  if (hours > 0) str += `${hours} hour${hours === 1 ? "" : "s"} `;
+  if (minutes > 0) str += `${minutes} minute${minutes === 1 ? "" : "s"} `;
+  if (seconds > 0) str += `${seconds} second${seconds === 1 ? "" : "s"} `;
+
+  return str.trim();
+};
 
 app.use(express.static("src/public"));
 
@@ -15,25 +39,49 @@ app.post("/create", urlencoded({ extended: true }), (req, res, next) => {
     (req.body?.minute || 0) * 60 * 1000 +
     (req.body?.second || 0) * 1000;
 
-  const timestamp = Date.now() + totalMs;
+  const now36 = nowWithOffset().toString(36);
+  const duration36 = Number(totalMs).toString(36);
 
-  res.redirect(`/${totalMs > 0 ? timestamp : ""}`);
+  if (totalMs <= 0) return res.redirect("/");
+
+  const time = `${now36}+${duration36}`;
+
+  res.redirect(`/${time}`);
 });
 
-app.get("/:timestamp", (req, res) => {
+app.get("/:timestamp", async (req, res) => {
   const { timestamp } = req.params ?? {};
+  const [start36, duration36] = timestamp?.split("+") ?? [];
+  const durationMs = Number.parseInt(duration36, 36);
+  const startMs = Number.parseInt(start36, 36) + START_DATE_CONSTRAINT;
+  const totalMs = startMs + durationMs;
+  const durationDate = new Date(durationMs);
 
-  if (new Date(Number(timestamp ?? NaN)).toString() === "Invalid Date") {
+  if (new Date(Number(totalMs ?? NaN)).toString() === "Invalid Date") {
     res.redirect("/");
   } else {
-    res.sendFile("src/timer.html", { root: process.cwd() });
+    const raw = await fs.readFile("src/timer.html", "utf-8");
+    const content = raw
+      .replace(/{{timestamp}}/g, timestamp)
+      .replace(/{{human-start-date}}/g, dayjs(startMs).format("DD MMM YYYY"))
+      .replace(/{{human-duration}}/g, formattedTime(durationMs));
+
+    res.setHeader("Content-Type", "text/html");
+    res.send(content);
   }
 });
 
-app.get("/preset/:minute-minutes", (req, res, next) => {
+app.get("/preset/:minute-minute(s)?", (req, res, next) => {
   if (!req.params?.minute) return next();
 
-  const timestamp = Date.now() + Number(req.params?.minute) * 60 * 1000;
+  const nowMs = nowWithOffset();
+  const minuteString = req.params?.minute === "a" ? "1" : req.params?.minute;
+  const durationMs = Number(minuteString) * 60 * 1000;
+
+  const now36 = Number(nowMs).toString(36);
+  const duration36 = Number(durationMs).toString(36);
+
+  const timestamp = `${now36}+${duration36}`;
 
   res.redirect(`/${timestamp}`);
 });
--- src/public/script.js
+++ src/public/script.js
@@ -1,8 +1,21 @@
+const START_DATE_CONSTRAINT = new Date("2023-02-09T00:00:00.000Z").getTime();
+
 const timestamp = window.location.pathname.slice(1);
-const timestampDate = new Date(Number(timestamp));
+
+const [start36, duration36] = timestamp?.split("+") ?? [];
+
+if (start36 === undefined || duration36 === undefined) {
+  window.location.href = "/";
+}
+
+const startTimestamp = Number.parseInt(start36, 36);
+const durationTimestamp = Number.parseInt(duration36, 36);
+const timestampDate = new Date(
+  START_DATE_CONSTRAINT + startTimestamp + durationTimestamp
+);
 
 if (
-  Number(timestamp) < Date.now() ||
+  timestampDate.getTime() < Date.now() ||
   timestampDate.toString() === "Invalid Date"
 ) {
   window.location.href = "/";
